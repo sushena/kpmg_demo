5603d5112d4742a79d43a7b5de442745ecc321f4
Sushena <sushena.p@gmail.com>
2021-09-24 09:09:11 +0100

    Update challenge_3.py

--- challenge_3.py
+++ challenge_3.py
@@ -1,24 +1,23 @@
-def findkeys(ojet, kv):
+def findkeys(node, kv):
     try:
-        if isinstance(ojet, list):
-            for i in ojet:
+        if isinstance(node, list):
+            for i in node:
                 if isinstance(i, dict):
                     for x in findkeys(i, kv):
                         return x
-        elif isinstance(ojet, dict):
-            if kv in ojet:
-                return ojet[kv]
-            for j in ojet.values():
-                for x in findkeys(j, kv):
-                    return x
+        elif isinstance(node, dict):
+            if kv in node:
+                return node[kv]
+            for j in node.values():
+                return findkeys(j, kv)
+                # for x in findkeys(j, kv):
+                #     return x
     except Exception as e:
         pass
 
 
-#d = {"a":{"b":{"c":"d"}}}
-#d = {"x":{"y":{"z":"a"}}}
-d = {"x":{"y":["z","a"]}}
-g = findkeys(d, 'y')
+d = {"a":{"b":["c","d"], "Z":{'a','b'}}}
+g = findkeys(d, 'b')
 if g is None:
     print('Key Missing')
 else:
